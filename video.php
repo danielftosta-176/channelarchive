c180039080625bf1f9a444127e65b9f1dfe41a0e
danielftosta-176 <danielftosta@hotmail.com>
2022-03-15 14:48:53 -0300

    e

--- video.php
+++ video.php
@@ -1,12 +1,51 @@
+
 <div class="video-container">
-      <video src="" width="640" height="360" ></video>
-      <h3>{{this.activeVideo.title}}</h3>
+      <?php
+            echo '<video controls src="/videos/';
+            echo $_GET['v'];
+            echo '.mp4" width="640" height="360"></video>';
+    ?>
+<?php
+echo '<h3>';
+echo $_GET['t'];
+echo '</h3>';
+//echo $_GET['v'];
+//echo $_POST['t'];
+?>
     <div class="row">
-        <p>{{this.activeVideo.views}} views</p> 
-        <p>{{this.activeVideo.likes}} <button>Like</button></p>
+        <p>nil views</p> 
+        <p>nil <button>Like</button></p>
     </div>
 </div>
+
+
 <style>
+.thumbnail{
+    display:flex;
+}
+
+.thumbnail img{
+    width:168px;
+}
+
+.thumbnail-info{
+    margin-left:20px;
+}
+
+.thumbnail h3{
+    font-size:16px;
+}
+
+h3,
+p{
+    margin:0;
+    padding:0;
+}
+
+.thumbnail-views{
+    font-size:14px;
+}
+
 .video-player{
     display:flex;
     width:1200px;
